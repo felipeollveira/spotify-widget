e050825dfc98b010f09425d1447deb800440fbc1
felipeollveira <masfelipe241@gmail.com>
2025-03-09 21:22:02 -0300

    initial commit -- hello world!

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace spotify_widget;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    
+}
+
